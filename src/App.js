6293f80222d5994a25881b044d6cb717466fe1ca
Heather White <miss.pomykal@gmail.com>
2022-03-17 18:52:47 -0400

    SETUP: first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './assets/smiley-face.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>Heather White</h3>
+        <p>
+          fullstack software engineer for hire y'all
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
