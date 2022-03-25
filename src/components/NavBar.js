1436e6b8ce8c41f6a4844982531ea92d85be15c0
Heather White <miss.pomykal@gmail.com>
2022-03-25 16:55:11 -0400

    UPDATE: import mui/styles, use makeStyles for nav & home

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,34 @@
 import React from "react";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 // import { useLocation } from "react-router-dom";
+import { makeStyles } from "@mui/styles";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import GitHubIcon from '@mui/icons-material/GitHub';
+import GitHubIcon from "@mui/icons-material/GitHub";
+import CodeIcon from "@mui/icons-material/Code";
+import LogoDevIcon from "@mui/icons-material/LogoDev";
+import HomeIcon from "@mui/icons-material/Home";
+
+/* MUI styles */
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 14,
+    backgroundColor: "black",
+    "&:hover": {
+      backgroundColor: "violet",
+    },
+    marginBottom: 20,
+    fontFamily: "Raleway",
+  },
+  logo: {
+    fontSize: 48,
+    color: "black",
+    "&:hover": {
+      color: "violet",
+    },
+  },
+});
+
+/* COMPONENT */
 const Navbar = () => {
   // const [path, setPath] = useState("");
   // const location = useLocation();
@@ -10,6 +36,7 @@ const Navbar = () => {
   // useEffect(() => {
   //   setPath(location.pathname);
   // }, [location]);
+  const classes = useStyles();
 
   return (
     <nav>
@@ -19,23 +46,41 @@ const Navbar = () => {
           target="_blank"
           rel="noreferrer"
         >
-          <LinkedInIcon />
+          <LinkedInIcon className={classes.logo} />
         </a>
-        <Button variant="outlined" color="secondary" href="/skills">
+        <Button
+          className={classes.btn}
+          variant="contained"
+          color="secondary"
+          href="/skills"
+          startIcon={<LogoDevIcon />}
+        >
           skills
         </Button>
       </div>
       <div>
-        <Button variant="outlined" color="secondary" href="/">
+        <Button
+          className={classes.btn}
+          variant="contained"
+          color="secondary"
+          href="/"
+          startIcon={<HomeIcon />}
+        >
           HTHR-WHT
         </Button>
       </div>
       <div>
-        <Button variant="outlined" color="secondary" href="/portfolio">
+        <Button
+          className={classes.btn}
+          variant="contained"
+          color="secondary"
+          href="/portfolio"
+          startIcon={<CodeIcon />}
+        >
           portfolio
         </Button>
         <a href="https://github.com/HTHR-WHT" target="_blank" rel="noreferrer">
-          <GitHubIcon />
+          <GitHubIcon className={classes.logo} />
         </a>
       </div>
     </nav>
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,18 +1,37 @@
 import React from "react";
+import { makeStyles } from "@mui/styles";
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Navbar from "../components/NavBar";
 
+/* MUI styles */
+const useStyles = makeStyles({
+  name: {
+    fontFamily: "Raleway",
+    fontWeight: 500,
+    letterSpacing: 5,
+    color: "black"
+  },
+  title: {
+    fontFamily: "Cabin",
+    letterSpacing: 15,
+    color: "black"
+  }
+});
+
+/* COMPONENT */
 const LandingPage = () => {
+  const classes = useStyles();
+
   return (
     <>
       <Container>
         <Navbar />
         <div>
-          <Typography variant="h1" color="secondary" align="center" gutterBottom>
+          <Typography className={classes.name} variant="h1" color="secondary" align="center" gutterBottom>
             Heather White
           </Typography>
-          <Typography variant="h4" color="secondary" align="center" gutterBottom>
+          <Typography className={classes.title} variant="h4" color="secondary" align="center" gutterBottom>
             software engineer
           </Typography>
         </div>
