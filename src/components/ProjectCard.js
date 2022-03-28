98681fc0c5a4a63e6a666f6c6fa385f23fe786b8
Heather White <miss.pomykal@gmail.com>
2022-03-28 13:10:04 -0400

    FEAT: card animations and more project info

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
 import { Typography } from "@mui/material";
 import { IconButton } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
+/* expand card helper function! thanks MUI docs :) */
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+/* COMPONENT */
 const ProjectCard = ({ project }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpand = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <div>
       <Card elevation={3}>
@@ -35,6 +59,24 @@ const ProjectCard = ({ project }) => {
         <CardContent>
           <Typography variant="body1">{project.description}</Typography>
         </CardContent>
+        <CardActions disableSpacing>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpand}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Tech Stack:</Typography>
+            <Typography paragraph>{project.tech}</Typography>
+            <Typography paragraph>Contributions:</Typography>
+            <Typography paragraph>{project.work}</Typography>
+          </CardContent>
+        </Collapse>
       </Card>
     </div>
   );
--- src/data/projects.js
+++ src/data/projects.js
@@ -8,7 +8,6 @@ import githubLogo from "../assets/icons8-github-50.png";
 import youtubeLogo from "../assets/icons8-youtube-50.png";
 import herokuLogo from "../assets/icons8-heroku-50.png";
 
-
 const projects = [
   {
     name: "City Diary",
@@ -16,11 +15,12 @@ const projects = [
     description:
       "City Diary is a mobile app that allows you to interact with and explore a user generated sound map.  Users can choose to publicly or privately share their audio on the City Diary Map, to revisit anytime.",
     tech: "React Native, Expo, Firebase",
+    work: "Set up Firebase connection and initialized app structure including sign up/login, rendering maps and navigation, styled app views, set up GitHub Project Board, readme.md, wiki documentation.",
     image: cdImg,
     github: githubLogo,
     repo: "HCJM/City-Diary",
     link: youtubeLogo,
-    path: "https://tinyurl.com/cityDiaryDemo"
+    path: "https://tinyurl.com/cityDiaryDemo",
   },
   {
     name: "Hot 'n' Saucy",
@@ -28,11 +28,12 @@ const projects = [
     description:
       "Hot 'N' Saucy is an e-commerce web app that provides customers with a variety of artisanal hot sauces. There are features for guests, returning customers with secure and persistent profiles, as well as administrators with access to product inventory and customer data.",
     tech: "Node.js, JavaScript, React, Redux, Axios, Express, pg, Sequelize, PostgreSQL, Heroku",
+    work: "Pair programmed design schema, database models, 'all products' view and cart functionality.  Created redux store for Admin, secure CRUD routes, and initial checkout view.",
     image: hotSauceImg,
     github: githubLogo,
     repo: "HotnSaucy/HotSauces",
     link: herokuLogo,
-    path: "https://hotnsaucy.herokuapp.com/"
+    path: "https://hotnsaucy.herokuapp.com/",
   },
   {
     name: "Starball",
@@ -40,11 +41,12 @@ const projects = [
     description:
       "Starball is a playful and creative coding exploration using p5.js.  Our friend, Starball, is a happy little sprite that loves to chase shiny things and munch on star candies.  However, all that star candy tends to make them act a little rowdy, to the chagrin of the alien sprites, their only neighbors in the sparkle cloud galaxy.",
     tech: "JavaScript, p5.js, p5 play library",
+    work: "Solo developer",
     image: starballImg,
     github: githubLogo,
     repo: "HTHR-WHT/p5-StarBall",
     link: youtubeLogo,
-    path: "https://youtu.be/OggzhuV33VI"
+    path: "https://youtu.be/OggzhuV33VI",
   },
 ];
 
